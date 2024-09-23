53bde06adf808be06fc8166b00baa8adba945202
ravi18 <ravi@arrowai.com>
2024-09-23 12:43:17 +0530

    changes

--- providers/anthropic/index.js
+++ providers/anthropic/index.js
@@ -46,68 +46,69 @@ class AnthropicHandler {
         });
     }
     async createCompletion(createParams) {
-        const modelId = createParams.model
-        switch (modelId) {
-            case "claude-3-5-sonnet-20240620":
-            case "claude-3-opus-20240229":
-            case "claude-3-haiku-20240307": {
-                const userMsgIndices = createParams.messages.reduce(
-                    (acc, msg, index) => (msg.role === "user" ? [...acc, index] : acc),
-                    []
-                );
-                const lastUserMsgIndex = userMsgIndices[userMsgIndices.length - 1] ?? -1;
-                const secondLastMsgUserIndex = userMsgIndices[userMsgIndices.length - 2] ?? -1;
-                const message = await this.client.beta.promptCaching.messages.create({
-                    model: modelId,
-                    max_tokens: this.getModel(modelId).info.maxTokens,
-                    system: [{ text: systemPrompt, type: "text", cache_control: { type: "ephemeral" } }],
-                    messages: messages.map((message, index) => {
-                        if (index === lastUserMsgIndex || index === secondLastMsgUserIndex) {
-                            return {
-                                ...message,
-                                content:
-                                    typeof message.content === "string"
-                                        ? [
-                                            {
-                                                type: "text",
-                                                text: message.content,
-                                                cache_control: { type: "ephemeral" },
-                                            },
-                                        ]
-                                        : message.content.map((content, contentIndex) =>
-                                            contentIndex === message.content.length - 1
-                                                ? { ...content, cache_control: { type: "ephemeral" } }
-                                                : content
-                                        ),
-                            };
-                        }
-                        return message;
-                    }),
-                    tools,
-                    tool_choice: { type: "auto" },
-                }, (() => {
-                    switch (modelId) {
-                        case "claude-3-5-sonnet-20240620":
-                            return {
-                                headers: {
-                                    "anthropic-beta": "prompt-caching-2024-07-31",
-                                },
-                            };
-                        case "claude-3-haiku-20240307":
-                            return {
-                                headers: { "anthropic-beta": "prompt-caching-2024-07-31" },
-                            };
-                        default:
-                            return undefined;
-                    }
-                })());
-                return { message };
-            }
-            default: {
-                const message = await this.client.messages.create(createParams);
-                return { message };
-            }
-        }
+        const modelId = createParams.model;
+        const message = await this.client.messages.create(createParams);
+        return { message };
+        // switch (modelId) {
+        //     case "claude-3-5-sonnet-20240620":
+        //     case "claude-3-opus-20240229":
+        //     case "claude-3-haiku-20240307": {
+        //         const userMsgIndices = createParams.messages.reduce(
+        //             (acc, msg, index) => (msg.role === "user" ? [...acc, index] : acc),
+        //             []
+        //         );
+        //         const lastUserMsgIndex = userMsgIndices[userMsgIndices.length - 1] ?? -1;
+        //         const secondLastMsgUserIndex = userMsgIndices[userMsgIndices.length - 2] ?? -1;
+        //         const message = await this.client.beta.promptCaching.messages.create({
+        //             model: modelId,
+        //             max_tokens: this.getModel(modelId).info.maxTokens,
+        //             system: [{ text: systemPrompt, type: "text", cache_control: { type: "ephemeral" } }],
+        //             messages: messages.map((message, index) => {
+        //                 if (index === lastUserMsgIndex || index === secondLastMsgUserIndex) {
+        //                     return {
+        //                         ...message,
+        //                         content:
+        //                             typeof message.content === "string"
+        //                                 ? [
+        //                                     {
+        //                                         type: "text",
+        //                                         text: message.content,
+        //                                         cache_control: { type: "ephemeral" },
+        //                                     },
+        //                                 ]
+        //                                 : message.content.map((content, contentIndex) =>
+        //                                     contentIndex === message.content.length - 1
+        //                                         ? { ...content, cache_control: { type: "ephemeral" } }
+        //                                         : content
+        //                                 ),
+        //                     };
+        //                 }
+        //                 return message;
+        //             }),
+        //             tools,
+        //             tool_choice: { type: "auto" },
+        //         }, (() => {
+        //             switch (modelId) {
+        //                 case "claude-3-5-sonnet-20240620":
+        //                     return {
+        //                         headers: {
+        //                             "anthropic-beta": "prompt-caching-2024-07-31",
+        //                         },
+        //                     };
+        //                 case "claude-3-haiku-20240307":
+        //                     return {
+        //                         headers: { "anthropic-beta": "prompt-caching-2024-07-31" },
+        //                     };
+        //                 default:
+        //                     return undefined;
+        //             }
+        //         })());
+        //         return { message };
+        //     }
+        //     default: {
+               
+        //     }
+        // }
     }
     getModel(modelId){
 		
